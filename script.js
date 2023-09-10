7aff130351188f810cba3232975af875c5befc84
priyajustin <124980916+priyajustin@users.noreply.github.com>
2023-09-10 20:14:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+var resume={
+    "basics": {
+      "name": "priyadharshini",
+      "label": "Programmer",
+      "image": "",
+      "email": "priya@gmail.com",
+      "phone": "6382175429",
+      "url": "https://priya.com",
+      "summary": "A summary of priya…",
+      "location": {
+        "address": "2712 Broadway St",
+        "postalCode": "CA 94115",
+        "city": "Tirupur",
+        "countryCode": "INDIA",
+        
+      },
+      "profiles": [{
+        "network": "Twitter",
+        "username": "priya",
+        "url": "https://twitter.com/priya"
+      }]
+    },
+    "work": [{
+      "name": "infotech",
+      "position": "hr",
+      "url": "https://infotech.com",
+      "startDate": "2013-01-01",
+      "endDate": "2014-01-01",
+      "summary": "Description…",
+      "highlights": [
+        "Started the company"
+      ]
+    }],
+    "volunteer": [{
+      "organization": "Organization",
+      "position": "Volunteer",
+      "url": "https://organization.com/",
+      "startDate": "2022-01-01",
+      "endDate": "2023-01-01",
+      "summary": "Description…",
+      "highlights": [
+        "Awarded 'Volunteer of the Month'"
+      ]
+    }],
+    "education": [{
+      "institution": "Bharathiyar university",
+      "url": "https://Bharathiyar university.com/",
+      "area": "Software Development",
+      "studyType": "Bachelor",
+      "startDate": "2022-01-01",
+      "endDate": "2023-01-01",
+      "score": "4.0",
+      "courses": [
+        "DB1101 - Basic SQL"
+      ]
+    }],
+    
+    "certificates": [{
+      "name":"web developetr",
+      "date": "2023-11-07",
+      "issuer": "guvi",
+      "url": "https://certificate.com"
+    }],
+    
+    "skills": [{
+      "name": "Web Development",
+      "level": "Master",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages": [{
+      "language": "English",
+      "fluency": "Native speaker"
+    }],
+    "interests": [{
+      "name": "Wildlife",
+      "keywords": [
+        "Ferrets",
+        "Unicorns"
+      ]
+    }],
+    "references": [{
+      "name": "Justin raj",
+      "reference": "Reference…"
+    }],
+    "projects": [{
+      "name": "clone youtube ",
+      "startDate": "2023-01-01",
+      "endDate": "2024-01-01",
+      "description": "Description...",
+      "highlights": [
+        "Won award at AIHacks 2016"
+      ],
+      "url": "https://project.com/"
+    }]
+  }
+  
